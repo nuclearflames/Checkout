ecd4a55eb097bdf22f6e12a1e3fa9470d9ca8abf
James <1993jamesgrant@gmail.com>
2020-08-19 21:38:17 +0100

    passing promocode tests

--- tests/promo_code.rb
+++ tests/promo_code.rb
@@ -8,11 +8,11 @@ describe PromoCode do
   end
 
   it "can accept a new promo code" do
-    expect(PromoCode.new(["Buy2OrMore", "ProdCodeLH"])).to have_attributes({ promo_codes: ["Buy2OrMore", "ProdCodeLH"] })
+    expect(PromoCode.new("Buy2OrMore", "ProdCodeLH")).to have_attributes(rulename: "Buy2OrMore", product_code: "ProdCodeLH")
   end
 
   it "rejects invalid promo code" do
-    expect(PromoCode.new(["WrongCode", "ProdCodeLH"])).to eq("Invalid configuration added")
+    expect(PromoCode.new("WrongCode", "ProdCodeLH").errors).to eq(["Invalid configuration added"])
   end
 
 end
