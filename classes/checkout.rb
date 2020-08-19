261940995bb422a012ceccfbe8064c1846abf64a
James <1993jamesgrant@gmail.com>
2020-08-19 22:35:12 +0100

    Add items to array (and add an extra check for array size)

--- classes/checkout.rb
+++ classes/checkout.rb
@@ -2,16 +2,24 @@ require './classes/promo_code.rb'
 
 class Checkout
 
-  attr_reader :promo_codes
+  attr_reader :promo_codes, :items
 
   def initialize(promo_codes_list = nil)
     @promo_codes = []
+    @items = []
 
     return if promo_codes_list.nil?
 
     promo_codes_list.each do |pc|
       @promo_codes.push(PromoCode.new(pc[0], pc[1]))
     end
+  end
+
+
+  def scan(item)
+    @items.push(item)
+
+    return({:code => item.code, :name => item.name, :price => item.price})
   end
 
   private
